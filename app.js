44dfa63a71009d03f20a0183d24b14c5608f3bb9
Padol Nitin <109783378+PadolNitin0509@users.noreply.github.com>
2022-07-27 17:02:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World! This node js file on git');
-});
-
-var server = app.listen(8080);
-console.log(" server is running at port 8080");
+}).listen(8080);
